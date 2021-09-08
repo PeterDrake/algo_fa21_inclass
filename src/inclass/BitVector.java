87275cb124a8befca7253bce02b2de079c614771
Peter Drake <drake@lclark.edu>
2021-09-08 13:39:07 -0700

    Most of BitVector.

---
+++ src/inclass/BitVector.java
@@ -0,0 +1,18 @@
+package inclass;
+
+public class BitVector {
+
+    private int data;
+
+    boolean get(int i) {
+        return ((data >>> i) & 1) == 1;
+    }
+
+    public void set(int i) {
+        data = data | (1 << i);
+    }
+
+    public void unset(int i) {
+
+    }
+}
---
+++ test/inclass/BitVectorTest.java
@@ -0,0 +1,29 @@
+package inclass;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BitVectorTest {
+
+    @Test
+    void initialBitIsOff() {
+        BitVector v = new BitVector();
+        assertFalse(v.get(7));
+    }
+
+    @Test
+    void setsBit() {
+        BitVector v = new BitVector();
+        v.set(4);
+        assertTrue(v.get(4));
+    }
+
+    @Test
+    void unsetsBit() {
+        BitVector v = new BitVector();
+        v.set(7);
+        v.unset(7);
+        assertFalse(v.get(7));
+    }
+}
